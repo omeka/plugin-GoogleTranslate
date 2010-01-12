454738454ae1cb864e62d2352dda378dd7c6aca4
Jeremy Boggs <jeremy@clioweb.org>
2010-01-12 14:59:38 +0000

    Adds google translate function to the public_theme_footer hook, so language can be changed on the front end.

--- plugin.php
+++ plugin.php
@@ -1,8 +1,9 @@
 <?php
 
-add_plugin_hook('admin_theme_footer', 'google_translate_admin_theme_footer');
+add_plugin_hook('admin_theme_footer', 'google_translate_theme_footer');
+add_plugin_hook('public_theme_footer', 'google_translate_theme_footer');
 
-function google_translate_admin_theme_footer()
+function google_translate_theme_footer()
 { ?>
 
 	<div id="google_translate_element"></div><script>
